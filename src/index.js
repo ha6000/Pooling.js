e919486696fe75712a3c753b36c1106c9d92a380
Non Hacker <krugerh27@gmail.com>
2020-04-13 18:04:31 +0200

    Added documentation

--- src/index.js
+++ src/index.js
@@ -4,28 +4,62 @@ const Keyv = require("keyv");
 const keyvExt = require("keyv-extensions");
 const shortid = require("shortid");
 shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ:.');
+/**
+ * A item instance used to interact with pool items
+ */
 class Item {
+    /**
+     * @param {Pool}   pool   The pool class
+     * @param {string} key    The key name of the item
+     * @param {string} status Status name the item belongs to
+     */
     constructor(pool, key, status) {
         this.pool = pool;
         this.key = key;
         this.status = status;
     }
+    /**
+     * Sets the value of the item
+     * @param  {any}           value
+     * @return {Promise<true>}       Returns promise, resolves to true if succesfull
+     */
     set(value) {
         return this.pool.set(this, value);
     }
+    /**
+     * Set the status of the
+     * @param  {string}        status
+     * @return {Promise<Item>}        Promise resolving with the new item
+     */
     setStatus(status) {
         return this.pool.setStatus(this, status);
     }
+    /**
+     * Gets the value of the item
+     * @return {Promise<any>} Promise with the value
+     */
     get() {
         return this.pool.get(this);
     }
 }
+/**
+ * Handles database and pools
+ */
 class Pool {
+    /**
+     * @param {Keyv} database The keyv database of choice
+     */
     constructor(database) {
         this.database = database;
         this.items = new Keyv({ store: new keyvExt.KeyvSub(this.database, 'data') });
         this.statuses = new Keyv({ store: new keyvExt.KeyvSub(this.database, 'statuses') });
     }
+    /**
+     * Add a item to the pool
+     * @param  {Item|Any}       value
+     * @param  {string}        status
+     * @return {Promise<Item>}        Promise with item
+     */
     add(value, status) {
         const key = value instanceof Item ? value.key : shortid.generate();
         status = value instanceof Item ? value.status : status;
@@ -40,10 +74,22 @@ class Pool {
             return new Item(this, key, status);
         });
     }
+    /**
+     * Sets a value of a key
+     * @param  {Item|string}      key
+     * @param  {any}           value
+     * @return {Promise<true>}       promise with true if succesfull
+     */
     set(key, value) {
         key = key instanceof Item ? key.key : key;
         return this.items.set(key, value);
     }
+    /**
+     * Sets status of a item
+     * @param  {Item|string}      key
+     * @param  {string}        status
+     * @return {Promise<Item>}        Promise with the new item
+     */
     setStatus(key, status) {
         const parsedkey = key instanceof Item ? key.key : key;
         return this.statuses.get(status)
@@ -56,14 +102,29 @@ class Pool {
             });
         });
     }
+    /**
+     * Get item's value
+     * @param  {Item|string}      key
+     * @return {Promise<any>}   The value
+     */
     get(key) {
         return this.items.get(key instanceof Item ? key.key : key);
     }
+    /**
+     * Get all the items with certain status
+     * @param  {string}          status
+     * @return {Promise<Item[]>}        Array with items
+     */
     getItemsOf(status) {
         return this.statuses.get(status).then(items => {
             return items.map(i => new Item(this, i, status));
         });
     }
+    /**
+     * get all of the value's with certain status
+     * @param  {string}         status
+     * @return {Promise<any[]>}        Array with all the values
+     */
     getOf(status) {
         var output = [];
         var resolve = (items) => {
--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,40 @@ import * as shortid from 'shortid';
 
 shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ:.')
 
+/**
+ * A item instance used to interact with pool items
+ */
 class Item {
+	/**
+	 * @param {Pool}   pool   The pool class
+	 * @param {string} key    The key name of the item
+	 * @param {string} status Status name the item belongs to
+	 */
 	constructor(pool: Pool, key: string, status: string) {
 		this.pool = pool;
 		this.key = key;
 		this.status = status;
 	}
+	/**
+	 * Sets the value of the item
+	 * @param  {any}           value
+	 * @return {Promise<true>}       Returns promise, resolves to true if succesfull
+	 */
 	set(value: any): Promise<true> {
 		return this.pool.set(this, value);
 	}
+	/**
+	 * Set the status of the 
+	 * @param  {string}        status
+	 * @return {Promise<Item>}        Promise resolving with the new item
+	 */
 	setStatus(status: string): Promise<Item> {
 		return this.pool.setStatus(this, status);
 	}
+	/**
+	 * Gets the value of the item
+	 * @return {Promise<any>} Promise with the value
+	 */
 	get(): Promise<any> {
 		return this.pool.get(this);
 	}
@@ -24,12 +46,24 @@ class Item {
 	status: string;
 }
 
+/**
+ * Handles database and pools
+ */
 class Pool {
+	/**
+	 * @param {Keyv} database The keyv database of choice
+	 */
 	constructor(database: Keyv) {
 		this.database = database;
 		this.items = new Keyv({store: new keyvExt.KeyvSub(this.database, 'data')});
 		this.statuses = new Keyv({store: new keyvExt.KeyvSub(this.database, 'statuses')});
 	}
+	/**
+	 * Add a item to the pool
+	 * @param  {Item|Any}       value
+	 * @param  {string}        status
+	 * @return {Promise<Item>}        Promise with item
+	 */
 	add(value: Item | any, status?: string): Promise<Item> {
 		const key = value instanceof Item ? value.key : shortid.generate();
 		status = value instanceof Item ? value.status : status;
@@ -44,10 +78,22 @@ class Pool {
 			return new Item(this, key, status);
 		});
 	}
+	/**
+	 * Sets a value of a key
+	 * @param  {Item|string}      key
+	 * @param  {any}           value
+	 * @return {Promise<true>}       promise with true if succesfull
+	 */
 	set(key: Item | string, value: any): Promise<true> {
 		key = key instanceof Item ? key.key : key;
 		return this.items.set(key, value);
 	}
+	/**
+	 * Sets status of a item
+	 * @param  {Item|string}      key
+	 * @param  {string}        status
+	 * @return {Promise<Item>}        Promise with the new item
+	 */
 	setStatus(key: Item | string, status: string): Promise<Item> {
 		const parsedkey = key instanceof Item ? key.key : key;
 		return this.statuses.get(status)
@@ -59,14 +105,29 @@ class Pool {
 			});
 		})
 	}
+	/**
+	 * Get item's value
+	 * @param  {Item|string}      key
+	 * @return {Promise<any>}   The value
+	 */
 	get(key: Item | string): Promise<any> {
 		return this.items.get(key instanceof Item ? key.key : key);
 	}
+	/**
+	 * Get all the items with certain status
+	 * @param  {string}          status
+	 * @return {Promise<Item[]>}        Array with items
+	 */
 	getItemsOf(status: string): Promise<Item[]> {
 		return this.statuses.get(status).then(items => {
 			return items.map(i => new Item(this, i, status));
 		})
 	}
+	/**
+	 * get all of the value's with certain status
+	 * @param  {string}         status
+	 * @return {Promise<any[]>}        Array with all the values
+	 */
 	getOf(status: string): Promise<any[]> {
 		var output = [];
 		var resolve = (items) => {
